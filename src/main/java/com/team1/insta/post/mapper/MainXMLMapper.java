ae8fa2829d3c4744fd3c6f88e8eff66e3f6a599f
seungmin <usm369@naver.com>
2021-10-22 20:41:06 +0900

    [ysm][post] 작업중

--- src/main/java/com/team1/insta/post/mapper/MainXMLMapper.java
+++ src/main/java/com/team1/insta/post/mapper/MainXMLMapper.java
@@ -8,8 +8,5 @@ import com.team1.insta.user.dto.User;
 public interface MainXMLMapper {
 	public List<Post> getPostList();
 	
-<<<<<<< HEAD
-=======
-	public User getUserInfo(Integer id);
->>>>>>> b08eb5da6ad6190b1f29b17bf4a6f87c4bb36ca4
+
 }
