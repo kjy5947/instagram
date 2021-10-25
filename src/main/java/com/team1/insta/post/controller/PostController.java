a6ac33d6e6c6ea83298e29f235726424dd76de6c
tnghk <tnghk@DESKTOP-5QHHHV2>
2021-10-25 15:57:24 +0900

    [KSH] 임시

--- src/main/java/com/team1/insta/post/controller/PostController.java
+++ src/main/java/com/team1/insta/post/controller/PostController.java
@@ -1,30 +1,36 @@
 package com.team1.insta.post.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.team1.insta.post.dao.mapper.PostMapper;
+import com.team1.insta.post.dto.Post;
+import com.team1.insta.user.dao.mapper.UserMapper;
+import com.team1.insta.user.dto.KeyConfirm;
+import com.team1.insta.user.dto.User;
 
-
-//@RequestMapping("/post")
-
-//@RestController
 
 @RequestMapping("/post")
 @Controller
 public class PostController {
 
-//	@GetMapping(value = "/getStr", produces = "text/html; charset=EUC-KR")
-//	public String getString() {
-//	    return "<h1>�ȳ��ϼ��� REST ��Ʈ�ѷ� �Դϴ�.</h1>";
-//	}
-
+	
+	@Autowired
+	PostMapper postMapper;
+	
+	@Autowired
+	UserMapper userMapper;
 	
 	
 //	@GetMapping(value ={"/personal", "/personal/*"})
-	@RequestMapping(value ={"/personal", "/personal/*"}, method= {RequestMethod.GET})
+	@RequestMapping(value ={"/personal"}, method= {RequestMethod.GET})
 	public String getString() {
 		
 	    return "post/personal";
@@ -36,4 +42,14 @@ public class PostController {
 	    return "post/upload";
 	}
 	
+	@GetMapping("/personal/uname")
+	public String userInfo(@RequestParam(value ="uname") String uname) {
+		
+		User user = userMapper.getUser(new KeyConfirm("", uname));
+		
+		List<Post> postList =  postMapper.getPostList(user.getUser_id());
+		
+		
+		return null;
+	}
 }
