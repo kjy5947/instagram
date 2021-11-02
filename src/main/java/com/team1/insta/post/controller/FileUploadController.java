aa9fb5a36834f6d34b4daf672655896b63c3be6d
Soyoung Baek <sybaek94@daum.net>
2021-11-02 19:10:35 +0900

    [soyoung][post] 게시물 올리기 작업중

--- src/main/java/com/team1/insta/post/controller/FileUploadController.java
+++ src/main/java/com/team1/insta/post/controller/FileUploadController.java
@@ -1,6 +1,8 @@
 package com.team1.insta.post.controller;
 
 import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
 
 import javax.servlet.http.Cookie;
@@ -60,9 +62,22 @@ public class FileUploadController {
 	    @RequestMapping(value = "/fileUpload/post") //ajax에서 호출하는 부분
 	    @ResponseBody
 	    public String upload(MultipartHttpServletRequest multipartRequest, HttpServletRequest request) { //Multipart로 받는다.
+	    	Cookie[] cookies = request.getCookies();
+	         
+	    	String uname = "";
+	    	for(Cookie cookie :cookies) {
+	    		if(cookie.getName().equals("sid")) {
+	    			uname = cookie.getValue();
+	    		}
+	    	}
+	    	String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+	    	
+	       postMapper.getNewPID(uname);
+		       
 	        Iterator<String> itr =  multipartRequest.getFileNames();
 	        
 	        String rootPath = request.getSession().getServletContext().getRealPath("/");
+	        rootPath = rootPath.replace("\\", "/");
 	        String attach_path = "resources/images/";
 //	        String filePath = "/Users/soyoung/Developer/instagram/src/main/webapp/resources/images"; //설정파일로 뺀다.
 	        
@@ -71,14 +86,12 @@ public class FileUploadController {
 	            MultipartFile mpf = multipartRequest.getFile(itr.next());
 	     
 	            String originalFilename = mpf.getOriginalFilename(); //파일명
-
-//	            String fileFullPath = filePath+"/"+originalFilename; //파일 전체 경로
-	            String fileFullPath = rootPath + attach_path + originalFilename; //파일 전체 경로
+	            String fileFullPath = rootPath + attach_path + uname + currentTime + "." + (originalFilename.split("\\.")[1]); //파일 전체 경로
 	     
 	            try {
 	                //파일 저장
 	                mpf.transferTo(new File(fileFullPath)); //파일저장 실제로는 service에서 처리
-	                
+
 	                System.out.println("originalFilename => "+originalFilename);
 	                System.out.println("fileFullPath => "+fileFullPath);
 	     
